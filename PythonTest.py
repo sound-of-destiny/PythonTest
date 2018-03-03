52638964d128c23dd844d5521b1563792dbaf389
shuchongchong <schong440@gmail.com>
2018-03-03 15:04:45 +0800

    test

--- PythonTest.py
+++ PythonTest.py
@@ -149,6 +149,22 @@ print("相加后的值为 : ", two_sum(10, 20))
 print("相加后的值为 : ", two_sum(20, 20))
 
 
+class MyClass:
+    """一个简单的类实例"""
+    i = 12345
+
+    def f(self):
+        return 'hello world'
+
+
+# 实例化类
+x = MyClass()
+
+# 访问类的属性和方法
+print("MyClass 类的属性 i 为：", x.i)
+print("MyClass 类的方法 f 输出为：", x.f())
+
+
 # input("\n\n按下 enter 键后退出。\n")
 
 
