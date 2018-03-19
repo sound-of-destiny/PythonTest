4f3465ef34463e7224e738eb6062d4a2838b9122
schong440 <schong440@gmail.com>
2018-03-19 22:48:16 +0800

    test

--- QS_Crawler.py
+++ QS_Crawler.py
@@ -1,6 +1,10 @@
-import re
-import requests
 import json
+import re
+
+import requests
+
+import MySQLdb
+
 
 def check_link(url):
     try:
@@ -11,8 +15,29 @@ def check_link(url):
     except:
         print('无法连接服务器')
 
+conn= MySQLdb.connect(
+        host='202.194.15.184',
+        port = 3306,
+        user='gradms',
+        passwd='gradms184.db',
+        db ='gradms'
+    )
+
+cur = conn.cursor() 
+
+i=0
 url = 'https://www.topuniversities.com/sites/default/files/qs-rankings-data/357051.txt?_=1521446318425'
 html = check_link(url)
 text = json.loads(html)
-for i in text["data"]:
-     print(i["title"])
+for rank in text["data"]:
+    i=i+1
+    print(rank["title"])
+    sql1="select nationId from gradms.base_nation where nationEnglishName='"+rank["country"].upper()+"'"
+    nationId=cur.execute(sql1)
+    sql2="insert into gradms.newabroad_university_info (nationId,universityName,orderNum,year) values (%d,%s,%d,%s)"
+    cur.execute(sql2,(nationId,rank["title"],i,'2018'))
+
+
+cur.close()
+conn.commit()
+conn.close()
